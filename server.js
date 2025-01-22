899411921094a72b620d881f5f0c1bc09db8a78a
Bella Solo <bella.solomon-falkfrii@outlook.com>
2025-01-22 11:19:42 +0100

    backend & API

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my chat");
+let messages = [];
+let currentId = 1;
+
+app.get("/api/messages", (req, res) => {
+  res.json(messages);
+  console.log("Welcome to my chat");
+});
+
+app.post("/api/messages", (req, res) => {
+  const { text } = req.body;
+  if (!text) {
+    return res.status(400);
+  }
+
+  const newMessage = {
+    id: currentId++,
+    text,
+    createdAt: new Date(),
+  };
+  messages.push(newMessage);
+  res.status(200).json(newMessage);
 });
 
 app.listen(PORT, () => {
-  console.log("listening to 3000");
+  console.log(`listening to PORT ${PORT}`);
 });
