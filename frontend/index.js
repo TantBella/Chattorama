b7359ed9683f73b9b23e5caab015200fce51f28a
Bella Solo <bella.solomon-falkfrii@outlook.com>
2025-01-22 15:08:04 +0100

    hopefully final version

--- frontend/index.js
+++ frontend/index.js
@@ -2,26 +2,48 @@ const messageDiv = document.getElementById("messages");
 const messageInput = document.getElementById("messageInput");
 const sendButton = document.getElementById("sendButton");
 
+// const localMessages = [];
+
+function getLocalMessages() {
+  const storedMessages = localStorage.getItem("localMessages");
+  return storedMessages ? JSON.parse(storedMessages) : [];
+}
+
+function saveLocalMessages(messages) {
+  localStorage.setItem("localMessages", JSON.stringify(messages));
+}
+
 async function loadMessages() {
   try {
     const response = await fetch(
       "https://cloud24chat.azurewebsites.net/api/messages"
     );
     const messages = await response.json();
-
+    const localMessages = getLocalMessages();
     messageDiv.innerHTML = "";
 
     messages.forEach((msg) => {
       const messageElement = document.createElement("div");
       messageElement.textContent = `${msg.text}`;
+
+      // if (msg.isLocalhost) {
+      //   messageElement.classList.add("message", "sent");
+      // } else {
+      //   messageElement.classList.add("message", "received");
+      // }
+
+      if (localMessages.includes(msg.text)) {
+        messageElement.classList.add("message", "sent");
+      } else {
+        messageElement.classList.add("message", "received");
+      }
       messageDiv.appendChild(messageElement);
     });
+    messageDiv.scrollTop = messageDiv.scrollHeight;
   } catch (error) {
     console.log(error);
   }
 }
-
-loadMessages();
 
 function sendMessage() {
   const input = messageInput.value.trim();
@@ -30,7 +52,10 @@ function sendMessage() {
     alert("Meddelandet kan inte vara tomt!");
     return;
   }
-
+  const localMessages = getLocalMessages();
+  localMessages.push(input);
+  saveLocalMessages(localMessages);
+  
   fetch("https://cloud24chat.azurewebsites.net/api/message", {
     method: "POST",
     headers: {
@@ -43,11 +68,22 @@ function sendMessage() {
       console.log("Meddelande skickat:", data);
 
       const messageElement = document.createElement("div");
-      messageElement.textContent = `${data.text}`;
+      messageElement.textContent = data.text;
+      messageElement.classList.add("message", "sent");
       messageDiv.appendChild(messageElement);
     })
     .catch((error) => {
       console.error("Fel vid skickande av meddelande:", error);
     });
   messageInput.value = "";
+  messageDiv.scrollTop = messageDiv.scrollHeight;
 }
+
+loadMessages();
+
+sendButton.addEventListener("click", sendMessage);
+messageInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    sendMessage();
+  }
+});
